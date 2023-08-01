b2c8d586340f7b01a64f0742019eab12c1e27202
Paul Berger <paulberger56@gmail.com>
2023-08-01 14:51:45 -0400

    Add submit functionality to the popup window and fix any bugs that arise.

--- Script.js
+++ Script.js
@@ -30,25 +30,12 @@ function removeBook(title) {
     }
 };
 
+// Hides the popup window
 function hidePopUp() {
     bookAdditionCard.style.display = 'none';
 };
 
-
-// Temporary books to show on the page
-let bookOne = new Book("Rain in Spain", "Writerino", 50, false);
-myLibrary.push(bookOne);
-
-let bookTwo = new Book("Save the drama for yo mamma", "You know who", 15, true);
-myLibrary.push(bookTwo);
-
-
-
-// Adds the books to the cards on the page whenever the array is updated
-
-const cardArea = document.querySelector(".card-container");
-
-myLibrary.forEach((book) => {
+function createCard(book) {
     const bookCard = document.createElement('div');
     bookCard.classList.add('card')
 
@@ -90,10 +77,27 @@ myLibrary.forEach((book) => {
         removeBook(titleToRemove);
         cardArea.removeChild(bookCard);
     });
-});
+};
+
+// Creates cards for every book inside myLibrary
+function generateAllCards() {
+    cardArea.innerHTML = '';
+    myLibrary.forEach((book) => {
+        createCard(book);
+    });
+};
+
+
+// Temporary books to show on the page
+let bookOne = new Book("Rain in Spain", "Writerino", 50, false);
+myLibrary.push(bookOne);
+
+let bookTwo = new Book("Save the drama for yo mamma", "You know who", 15, true);
+myLibrary.push(bookTwo);
 
 
 // Selection of parts of the DOM
+const cardArea = document.querySelector(".card-container");
 const addButton = document.querySelector(".add-button");
 const bookAdditionCard = document.querySelector('.book-addition-card');
 const submitButton = document.querySelector('.submit');
@@ -105,8 +109,39 @@ addButton.addEventListener('click', () => {
 });
 
 // Hides the popup window when cancel is pressed inside of it
-cancelButton.addEventListener('click', hidePopUp());
-
-submitButton.addEventListener('click', () => {
-
+cancelButton.addEventListener('click', () => {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('pages').value = '';
+    document.getElementById('yes').checked = true;
+    hidePopUp();
 });
+
+// Creates a new book and adds it to myLibrary when submit is pressed
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = parseInt(document.getElementById('pages').value);
+    const isRead = document.getElementById('yes').checked;
+
+    const newBook = new Book(title, author, pages, isRead);
+    addBookToLibrary(newBook);
+
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('pages').value = '';
+    document.getElementById('yes').checked = true;
+
+    createCard(newBook);
+
+    hidePopUp();
+});
+
+// Calls generateAllCards to populate the hardcoded cards in the beginning
+generateAllCards();
+
+
+
+
